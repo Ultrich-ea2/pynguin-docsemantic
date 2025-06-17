064e14f5847e4423fe61b93b38a4ece994dc961e
ultrich-ea2 <ultrichedima@gmail.com>
2025-06-17 14:19:57 +0200

    test(semantics): cover constraint & example extraction

---
+++ src/pynguin/tests/semantics/test_docstring_extractor.py
@@ -0,0 +1,14 @@
+from pynguin.semantics.docstring_extractor import semantics_for
+
+def test_constraints_and_examples():
+    def foo(x: int, y: int):
+        """Demo.
+
+        Args:
+            x: must be > 0. Example: foo(3, 0)
+            y: any int
+        """
+    sem = semantics_for(foo)
+    assert sem is not None
+    assert sem.params["x"].constraint == "x > 0"
+    assert 3 in sem.params["x"].example_values
