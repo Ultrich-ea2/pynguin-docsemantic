15e3d0c5cb7d253e4889221f019fd590e8f53e7b
tb-2000 <tomblum@gmx.de>
2025-06-22 13:11:35 +0200

    add copyright/licensing information

--- src/pynguin/semantics/__init__.py
+++ src/pynguin/semantics/__init__.py
@@ -1,3 +1,9 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2025 Pynguin Contributors
+#
+#  SPDX-License-Identifier: MIT
+#
 """Semantik-Erweiterungen für Pynguin (Docstrings etc.)."""
 from .docstring_extractor import (
     semantics_for,
